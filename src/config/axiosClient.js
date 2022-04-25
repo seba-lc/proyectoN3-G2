e99099a6c91ed2f44175a04ed2fd834285af2fe5
Sebastian Lopez Cruz <sebalopezx@gmail.com>
2022-04-25 11:56:23 -0300

    cambio en axiosConfig

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL:'https://encuestas21i-back.herokuapp.com/api/encuestas'
+  baseURL: 'http://localhost:4000/api/encuestas'
 })
 
 export default axiosClient;
 
 //'http://localhost:4000/api/encuestas'
+//'https://encuestas21i-back.herokuapp.com/api/encuestas'
